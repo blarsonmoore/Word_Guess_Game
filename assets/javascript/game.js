42740011d1f740c77417c17663ff770aa249395f
blarsonmoore <blarsonmoore@gmail.com>
2018-04-30 07:35:55 -0600

    add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,20 @@
 var words = ["Adult", "Aeroplane", "Air", "Aircraft Carrier", "Airforce", "Airport", "Album", "Alphabet", "Apple", "Arm", "Army", "Baby", "Baby", "Backpack"]
+var randomWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
+var wordLen = randomWord.length;
 
 
 
-function wordFunction() {
-  var randomWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
+console.log(randomWord);
+ console.log(wordLen);
 
-  console.log(randomWord);
- 
+ for (var i=0; i < randomWord.length; i++) {
+   wordLen[i] = "_ ";
+ }
+ document.getElementById("displayWord").innerHTML = wordLen[i];
+//Displays randomWord on screen
+  // var html = "<p>" + randomWord + "</p>";
+  // document.querySelector('#displayWord').innerHTML = html;
 
-//Displays random word on screen
-  var html = "<p>" + randomWord + "</p>";
-  document.querySelector('#displayWord').innerHTML = html;
-}
+  // function wordFunction() {
+  //   var randomWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
+  // }
