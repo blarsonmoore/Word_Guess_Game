15e3a68e9604929ecd69d8a08b22c2455f5d23aa
blarsonmoore <blarsonmoore@gmail.com>
2018-05-01 15:57:41 -0600

    working prototype

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,26 @@
+//Global Variables
 var words = ["Adult", "Aeroplane", "Air", "Aircraft Carrier", "Airforce", "Airport", "Album", "Alphabet", "Apple", "Arm", "Army", "Baby", "Baby", "Backpack"]
 var randomWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
 var wordLen = randomWord.length;
-
+var displayLen = [wordLen];
+var output = "";
+var userGuess = "";
 
 
 console.log(randomWord);
- console.log(wordLen);
+console.log(wordLen);
 
- for (var i=0; i < randomWord.length; i++) {
-   wordLen[i] = "_ ";
- }
- document.getElementById("displayWord").innerHTML = wordLen[i];
-//Displays randomWord on screen
-  // var html = "<p>" + randomWord + "</p>";
-  // document.querySelector('#displayWord').innerHTML = html;
+//For loop to display _ for each letter
 
-  // function wordFunction() {
-  //   var randomWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
-  // }
+for (var i = 0; i < randomWord.length; i++) {
+  displayLen[i] = "_ ";
+  output = output + displayLen[i];
+}
+document.getElementById("displayWord").innerHTML = output;
+output = "";
+
+// function click (randomWord) {
+//   document.getElementById("clickMe").onclick;
+// }
+
+// document.getElementById("clickMe").onclick
